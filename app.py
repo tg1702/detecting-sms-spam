76c7221db4250ca5fc55cd4099106cf7db42ca13
tg1702 <65041408+tg1702@users.noreply.github.com>
2023-12-22 17:09:59 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ from functions import transform_text
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score
 
+st.set_page_config(page_title='Text Message Spam Classifier', layout='wide', initial_sidebar_state='auto')
+
 cv = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
+
+# favicon =
+
+
 
 st.title("Text Message Spam Classifier")
 
@@ -27,23 +33,24 @@ if sms != "":
     else:
         st.subheader("Your message was detected as :green[Safe]")
 
-dataset_url = "https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset"
+dataset_url = "https://archive.ics.uci.edu/dataset/228/sms+spam+collection"
 github_url = "https://github.com/tg1702/detecting-sms-spam"
 colab_url = "https://colab.research.google.com/drive/18ZqnOXJ0KCw6PDuNiV1FioJGFyAjsVkG?usp=sharing"
+kaggle_url = "https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset"
 
 st.header("Why Spam?")
-st.markdown(" Many of us are familiar with receiving spam messages via Whatsapp, Instagram or other social media platforms"
-            " By training a model on a Kaggle dataset of spam and ham (non-spam) messages, we can detect whether a message is spam or not."
-            " Spam messages contain certain frequently-used keywords, helping a program to discern them from normal messages."
-            "Here are some of the most common spam keywords:")
+st.markdown(
+    " Many of us are familiar with receiving spam messages via Whatsapp, Instagram or other social media platforms"
+    " By training a model on a Kaggle dataset of spam and ham (non-spam) messages, we can detect whether a message is spam or not."
+    " Spam messages contain certain frequently-used keywords, helping a program to discern them from normal messages."
+    "Here are some of the most common spam keywords:")
 
 st.image("keywords.png")
 st.caption("Word cloud showing most common spam words. See :link[%s] for more " % colab_url)
 
-
 st.header("About Project")
 
-
-st.markdown("Dataset: %s" % dataset_url)
+st.markdown("Original dataset available at: %s" % dataset_url)
+st.markdown("Kaggle .csv file: %s" % kaggle_url)
 st.markdown("See all of my code on my Github: %s " % github_url)
 st.markdown("Colab file: %s" % colab_url)
